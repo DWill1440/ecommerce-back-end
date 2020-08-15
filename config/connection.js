c692169c6bd6d4f53ed0e9e3d43f1a346b329ed4
DWill1440 <dwill2think@gmail.com>
2020-08-14 20:58:05 -0500

    columns and associations

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
-
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
       dialect: 'mysql',
+      port: 3301,
       dialectOptions: {
         decimalNumbers: true,
       },
